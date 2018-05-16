3965970baa06a5bf41b2bf15618f04e053f58485
Nadim <syed.m.nadim@gmail.com>
2018-05-16 08:54:10 -0400

    first commit

---
+++ Lab18-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.AddAtStart("Adam");
+            list.AddAtStart("Adam");
+            list.AddAtStart("Cat");
+            list.AddAtStart("Mohammed");
+            list.AddAtStart("Shaun");
+            list.AddToEnd("Danny");
+            list.AddToEnd("Jaide");
+
+            //list.PrintAllNodes();
+            //list.RemoveFromStart();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //list.PrintAllNodes();
+            //list.RemoveAllFromStart(list);
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    list.RemoveFromStart();
+            //}
+            Console.WriteLine();
+            Console.WriteLine();
+            list.PrintAllNodes();
+            Console.WriteLine();
+            Console.WriteLine();
+            list.RemoveAllObjectO("Adam");
+            list.PrintAllNodes();
+        }
+    }
+}
